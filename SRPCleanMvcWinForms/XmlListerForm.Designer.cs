63740f5e5021fd0675e42c048d6c1fd19d1e2b03
Mert Akdut <mert.akdut@gmail.com>
2019-02-17 00:25:45 +0300

    initialization with dirty code.

---
+++ SRPCleanMvcWinForms/XmlListerForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SRPCleanMvcWinForms
+{
+    partial class XmlListerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ProductId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProductName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProductUnitPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ProductDiscontinued = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(713, 27);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ProductId,
+            this.ProductName,
+            this.ProductUnitPrice,
+            this.ProductDiscontinued});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 74);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 349);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ProductId
+            // 
+            this.ProductId.Text = "Id";
+            this.ProductId.Width = 91;
+            // 
+            // ProductName
+            // 
+            this.ProductName.Text = "Name";
+            this.ProductName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ProductName.Width = 229;
+            // 
+            // ProductUnitPrice
+            // 
+            this.ProductUnitPrice.Text = "UnitPrice";
+            this.ProductUnitPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ProductUnitPrice.Width = 356;
+            // 
+            // ProductDiscontinued
+            // 
+            this.ProductDiscontinued.Text = "Discontinued";
+            this.ProductDiscontinued.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ProductDiscontinued.Width = 93;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 27);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(93, 27);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(614, 20);
+            this.txtFileName.TabIndex = 3;
+            // 
+            // XmlListerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "XmlListerForm";
+            this.Text = "XmlListerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.ColumnHeader ProductId;
+        private System.Windows.Forms.ColumnHeader ProductName;
+        private System.Windows.Forms.ColumnHeader ProductUnitPrice;
+        private System.Windows.Forms.ColumnHeader ProductDiscontinued;
+    }
+}
+
---
+++ SRPCleanMvcWinForms/XmlListerForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SRPCleanMvcWinForms
+{
+    public partial class XmlListerForm : Form
+    {
+        public XmlListerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "XML Document (*.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog1.InitialDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\"));
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFileName.Text = openFileDialog1.FileName;
+                btnLoad.Enabled = true;
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            var fileName = txtFileName.Text;
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                var reader = XmlReader.Create(fs);
+                while (reader.Read())
+                {
+                    if (reader.Name != "product") continue;
+                    var id = reader.GetAttribute("id");
+                    var name = reader.GetAttribute("name");
+                    var unitPrice = reader.GetAttribute("unitPrice");
+                    var discontinued = reader.GetAttribute("discontinued");
+                    var item = new ListViewItem(new string[] { id, name, unitPrice, discontinued });
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+    }
+}
